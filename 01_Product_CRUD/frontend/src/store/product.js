50a73075b6391b7812e73c4c22f85c788ec02fb5
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-16 02:42:16 +0600

    read and delete product frontend done

--- 01_Product_CRUD/frontend/src/store/product.js
+++ 01_Product_CRUD/frontend/src/store/product.js
@@ -1,21 +1,38 @@
-import {create} from "zustand"
+import { create } from "zustand"
 
-export const useProductStore = create((set)=>({
+export const useProductStore = create((set) => ({
     products: [],
-    setProducts: (products) => set ({products}),
-    createProduct: async (newProduct) =>{
-        if(!newProduct.name || !newProduct.image || !newProduct.price){
-            return {success: false, message: "Please fill in all fiends."}
+    setProducts: (products) => set({ products }),
+    createProduct: async (newProduct) => {
+        if (!newProduct.name || !newProduct.image || !newProduct.price) {
+            return { success: false, message: "Please fill in all fiends." }
         }
         const res = await fetch("/api/products", {
             method: "POST",
-            headers:{
+            headers: {
                 "content-type": "application/json"
             },
             body: JSON.stringify(newProduct)
         })
         const data = await res.json();
-        set((state)=>({products:[...state.products,data.data]}));
-        return {success: true, message: "Product created successfully."};
+        set((state) => ({ products: [...state.products, data.data] }));
+        return { success: true, message: "Product created successfully." };
+    },
+
+    fetchProducts: async () => {
+        const res = await fetch("/api/products");
+        const data = await res.json();
+        set({ products: data.data });
+    },
+
+    deleteProduct: async (pid) => {
+        const res = await fetch(`/api/products/${pid}`, {
+            method: "DELETE",
+        });
+        const data = await res.json();
+        if (!data.success) return { success: false, message: data.message };
+
+        set((state) => ({ products: state.products.filter(product => product._id !== pid) }));
+        return { success: true, message: data.message };
     }
 }))
